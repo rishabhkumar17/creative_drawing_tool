51e33ca6c94024ee926d83c9829138ec355097fa
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-06 08:09:43 +0530

    backend server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { Server } = require('http')
+const app = express()
+
+const server = require('http').createServer(app)
+const { server } = require('socket.io')
+
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.send('This is creative drawing tool app official server by Rishabh Kumar')
+})
+
+const port = process.env.PORT || 5000
+server.listen(port, () =>
+  console.log('server is running on http://localhost:5000')
+)
