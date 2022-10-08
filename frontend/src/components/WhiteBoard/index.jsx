ae4d5a0b73de246681cc0526d3006e51f2e48f0e
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-09 02:45:51 +0530

    Pencil working

--- frontend/src/components/WhiteBoard/index.jsx
+++ frontend/src/components/WhiteBoard/index.jsx
@@ -3,11 +3,13 @@ import rough from 'roughjs'
 
 const roughGenerator = rough.generator()
 
-const WhiteBoard = ({ canvasRef, contextRef, elements, setElements }) => {
+const WhiteBoard = ({ canvasRef, contextRef, elements, setElements, tool }) => {
   const [isDrawing, setIsDrawing] = useState(false)
 
   useEffect(() => {
     const canvas = canvasRef.current
+    canvas.height = window.innerHeight * 2
+    canvas.width = window.innerWidth * 2
     const context = canvas.getContext('2d')
 
     contextRef.current = context
@@ -17,7 +19,16 @@ const WhiteBoard = ({ canvasRef, contextRef, elements, setElements }) => {
     const roughCanvas = rough.canvas(canvasRef.current)
 
     elements.forEach((element) => {
-      roughCanvas.linearPath(element.path)
+      if (element.type === 'pencil') {
+        roughCanvas.linearPath(element.path)
+      } else if (element.type === 'line') {
+        roughGenerator.line(
+          element.offsetX,
+          element.offsetY,
+          element.width,
+          element.height
+        )
+      }
     })
   }, [elements])
 
@@ -25,37 +36,68 @@ const WhiteBoard = ({ canvasRef, contextRef, elements, setElements }) => {
     const { offsetX, offsetY } = e.nativeEvent
     // console.log(offsetX, offsetY)
 
-    setElements((prevElements) => [
-      ...prevElements,
-      {
-        type: 'pencil',
-        offsetX,
-        offsetY,
-        path: [[offsetX, offsetY]],
-        storke: 'black',
-      },
-    ])
+    if (tool === 'pencil') {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: 'pencil',
+          offsetX,
+          offsetY,
+          path: [[offsetX, offsetY]],
+          storke: 'black',
+        },
+      ])
+    } else if (tool === 'line') {
+      setElements((prevElements) => [
+        ...prevElements,
+        {
+          type: 'line',
+          offsetX,
+          offsetY,
+          width: offsetX,
+          height: offsetY,
+          storke: 'black',
+        },
+      ])
+    }
+
     setIsDrawing(true)
   }
 
   const handleMouseMove = (e) => {
     const { offsetX, offsetY } = e.nativeEvent
-    if (isDrawing) {
-      const { path } = elements[elements.length - 1]
-      const newPath = [...path, [offsetX, offsetY]]
 
-      setElements((prevElements) =>
-        prevElements.map((ele, index) => {
-          if (index === elements.length - 1) {
-            return {
-              ...ele,
-              path: newPath,
+    if (isDrawing) {
+      if (tool === 'pencil') {
+        const { path } = elements[elements.length - 1]
+        const newPath = [...path, [offsetX, offsetY]]
+        setElements((prevElements) =>
+          prevElements.map((ele, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...ele,
+                path: newPath,
+              }
+            } else {
+              return ele
             }
-          } else {
-            return ele
-          }
-        })
-      )
+          })
+        )
+      } else if (tool === 'line') {
+        setElements((prevElements) =>
+          prevElements.map((ele, index) => {
+            if (index === elements.length - 1) {
+              return {
+                ...ele,
+                width: offsetX,
+                height: offsetY,
+              }
+            } else {
+              return ele
+            }
+          })
+        )
+      }
     }
   }
 
@@ -64,13 +106,14 @@ const WhiteBoard = ({ canvasRef, contextRef, elements, setElements }) => {
   }
 
   return (
-    <canvas
-      ref={canvasRef}
+    <div
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
-      className="border border-dark border-3 h-100 w-100"
-    ></canvas>
+      className="border border-dark border-3 h-100 w-100 overflow-hidden"
+    >
+      <canvas ref={canvasRef}></canvas>
+    </div>
   )
 }
 
