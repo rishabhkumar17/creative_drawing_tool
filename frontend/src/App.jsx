91bb9a4c214569f2f266954e5668c1fe62ffa08a
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-08 05:44:11 +0530

    Room page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import Forms from './components/Forms/index'
+import RoomPage from './components/WhiteBoard/index'
+import { Route, Routes } from 'react-router-dom'
+
 import './App.css'
 
 const App = () => {
   return (
     <div className="container">
-      <Forms/>
+      <Routes>
+        <Route path="/" element={<Forms />} />
+        <Route path="/:roomId" element={<RoomPage />} />
+      </Routes>
     </div>
   )
 }
