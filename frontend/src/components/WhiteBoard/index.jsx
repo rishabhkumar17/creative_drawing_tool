3168135c2fe3c4b1994843abc39ab9d5e0ef149b
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-08 05:42:42 +0530

    color

--- frontend/src/components/WhiteBoard/index.jsx
+++ frontend/src/components/WhiteBoard/index.jsx
@@ -3,18 +3,20 @@ import { useState } from 'react'
 
 const RoomPage = () => {
   const [tool, setTool] = useState('pencil')
+  const [colot, setColor] = useState('black')
   return (
     <div className="row">
       <h1 className="text-center py-5">White Board Sharing App</h1>
-      <div className="col-md-12 mt-4 mb-5 d-flex align-items-center justify-content-around">
-        <div className="d-flex col-md-4 justify-content-between gap-1">
-          <div className="d-flex gap-1">
+      <div className="col-md-12 px-5 mt-4 mb-5 d-flex align-items-center">
+        <div className="d-flex col-md-2 justify-content-between gap-1">
+          <div className="d-flex gap-1 align-items-center">
             <label htmlFor="pencil">Pencil</label>
             <input
               type="radio"
               id="pencil"
               name="tool"
               value="pencil"
+              className="mt-1"
               onChange={(e) => setTool(e.target.value)}
             />
           </div>
@@ -25,6 +27,7 @@ const RoomPage = () => {
               id="line"
               name="tool"
               value="line"
+              className="mt-1"
               onChange={(e) => setTool(e.target.value)}
             />
           </div>
@@ -35,7 +38,19 @@ const RoomPage = () => {
               id="rect"
               name="tool"
               value="rect"
+              className="mt-1"
               onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="col-md-7">
+          <div className="d-flex flex-column align-items-center">
+            <label htmlFor="color">Select Color:</label>
+            <input
+              type="color"
+              id="color"
+              className="mt-1"
+              onChange={(e) => setColor(e.target.value)}
             />
           </div>
         </div>
