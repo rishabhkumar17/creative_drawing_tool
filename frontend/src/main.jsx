7fee6eeea72928c8aac3e2d68816a3ff280082ae
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-08 05:44:45 +0530

    Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min"; 
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 )
