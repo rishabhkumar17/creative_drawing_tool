02bda19fc207d6f87f3cfb91c88e809d89e305d8
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-10 05:52:27 +0530

    uuid function for random roomId generate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,35 @@
 import Forms from './components/Forms/index'
 import RoomPage from './pages/RoomPage/index'
+
 import { Route, Routes } from 'react-router-dom'
 
 import './App.css'
+
+const uuid = () => {
+  let S4 = () => {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1)
+  }
+  return (
+    S4() +
+    S4() +
+    '-' +
+    S4() +
+    '-' +
+    S4() +
+    '-' +
+    S4() +
+    '-' +
+    S4() +
+    S4() +
+    S4()
+  )
+}
 
 const App = () => {
   return (
     <div className="container">
       <Routes>
-        <Route path="/" element={<Forms />} />
+        <Route path="/" element={<Forms uuid={uuid} />} />
         <Route path="/:roomId" element={<RoomPage />} />
       </Routes>
     </div>
