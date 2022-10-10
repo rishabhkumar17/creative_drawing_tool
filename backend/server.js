9f13247e3ad375b6312ce321b920e72fd16832d6
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-10 10:15:29 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,39 @@
 const express = require('express')
-const { Server } = require('http')
 const app = express()
 
 const server = require('http').createServer(app)
-const { server } = require('socket.io')
+const { Server } = require('socket.io')
+
+const { addUser } = require('./utils/users')
 
 const io = new Server(server)
 
 app.get('/', (req, res) => {
-  res.send('This is creative drawing tool app official server by Rishabh Kumar')
+  res.send('This is creative drawing tool app server by Rishabh Kumar')
 })
 
+let roomIdGlobal, imgURLGlobal
+
 io.on('connection', (socket) => {
-  console.log('user connected')
+  socket.on('userJoined', (data) => {
+    const { name, userId, roomId, host, presenter } = data
+    roomIdGlobal = roomId
+    socket.join(roomId)
+    const users = addUser(data)
+    socket.emit('userIsJoined', { success: true, users })
+    socket.broadcast.to(roomId).emit('allUsers', users)
+    socket.broadcast.to(roomId).emit('whiteBoardDataResponse', {
+      imgURL: imgURLGlobal,
+    })
+  })
+  socket.on('whiteBoardData', (data) => {
+    imgURLGlobal = data
+    socket.broadcast.to(roomIdGlobal).emit('whiteBoardDataResponse', {
+      imgURL: data,
+    })
+  })
 })
+
 const port = process.env.PORT || 5000
 
 server.listen(port, () =>
